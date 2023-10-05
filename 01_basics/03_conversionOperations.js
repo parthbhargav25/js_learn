da63122737090339756293616ac92afad6944338
Parth Bhargav <132897056+parthbhargav25@users.noreply.github.com>
2023-10-05 12:46:31 +0000

    new files added

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,22 @@
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
